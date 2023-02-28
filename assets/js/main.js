e263918d57d87401b3352f8c0e0784d238182703
rrrlong <danielsavatin@gmail.com>
2023-02-28 13:09:54 +0300

    add assets and index from dist to root for hosting

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const headerElem = document.querySelector("#header-component")
+
+const togleMenu = () => {
+  headerElem.classList.toggle("active")
+}
+
+const closeMenu = () => {
+  headerElem.classList.remove("active")
+}
+const navLinks = document.querySelectorAll("#nav-list a")
+const allNavBlocks = document.querySelectorAll(".nav_block")
+
+window.addEventListener("scroll", () => {
+  closeMenu()
+
+  let currentScrollPos = document.documentElement.scrollTop
+
+  if (currentScrollPos + 200 < allNavBlocks[0].offsetTop) {
+    navLinks.forEach((navLink) => {
+      navLink.classList.remove("active")
+    })
+  } else {
+    allNavBlocks.forEach((navBlock, blockIndex) => {
+      if (currentScrollPos + 200 > navBlock.offsetTop) {
+        if (blockIndex > 0) {
+          navLinks[blockIndex - 1].classList.remove("active")
+        }
+        if (blockIndex + 1 < navLinks.length) {
+          navLinks[blockIndex + 1].classList.remove("active")
+        }
+        navLinks[blockIndex].classList.add("active")
+        return
+      }
+    })
+  }
+})
